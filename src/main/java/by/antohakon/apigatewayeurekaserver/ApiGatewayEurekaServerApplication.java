28310487ccc3ce1abd3941841fb851a01e454a76
AntonP17 <pokrovskii.anton@mail.ru>
2025-10-22 20:00:03 +0300

    create server eureka

---
+++ src/main/java/by/antohakon/apigatewayeurekaserver/ApiGatewayEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package by.antohakon.apigatewayeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ApiGatewayEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayEurekaServerApplication.class, args);
+    }
+
+}
